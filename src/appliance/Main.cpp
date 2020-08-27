18814354299e28694745c11d014635842ae755ac
Yowgf <alexthomasmol@gmail.com>
2020-08-27 11:42:14 -0300

    Basic structure
    Added basic includes and explanation to Main file.

--- src/appliance/Main.cpp
+++ src/appliance/Main.cpp
@@ -1,5 +1,17 @@
-// ALG1-TP1 project ~Main~ file
-///////////////////////////////
+// ALG1-TP1 project Main file
+// Main function
+// ============================
+//
+// Description:
+// Direct interaction with the world outside of
+//   the program, and transfers control to the
+//   most high-level JPI module routine.
+// Only parameter passing and simple exception
+//   handling is done in the main function.
+/////////////////////////////////////////////////
+
+#include "Containers/Containers.hpp"
+#include "JPI/JPI.hpp"
 
 int main()
 {return 0;}
