f1355524d53cbcd3d181e495cb3b40a81122237c
Yowgf <alexthomasmol@gmail.com>
2020-08-29 13:00:31 -0300

    Class description

---
+++ src/header/Containers/players.hpp
@@ -0,0 +1,22 @@
+// ALG1-TP1 project header file
+// Containers module
+// Players
+// ============================
+//
+// Description:
+// Class that implements the set of all players
+//   currently active in the game. Provides
+//   manipulation routines that involve the all
+//   the players.
+/////////////////////////////////////////////////
+
+#ifndef PLAYERS_H
+#define PLAYERS_H 1
+
+namespace Containers {
+
+class players {};
+
+}
+
+#endif
