359832c2e65f94c003d79376026bc69f10d2dd2e
Yowgf <alexthomasmol@gmail.com>
2020-08-29 12:55:24 -0300

    First version
    Two structures were created to facilitate argument transfering, and the class adapted to the existance of the new "players" class.
    
    The main idea of this class is simply to initialize the main objects, and hand them to the manager.

--- src/header/JPI/init.hpp
+++ src/header/JPI/init.hpp
@@ -14,22 +14,35 @@
 #define INIT_H 1
 
 #include "Containers/board.hpp"
-#include "Containers/player.hpp"
+#include "Containers/players.hpp"
 
 #include "JPI/manager.hpp"
 
-#include <list>
+#include <vector>
+#include <utility>
 
 namespace JPI {
 
 class init {
 private:
-	Containers::board* init_board();
-	std::list<Containers::player*>* init_players();
+	struct m_aux_matrix{
+		int m, n;
+		char** entries;
+	};
+
+	struct m_separated_arg {
+		m_aux_matrix* board;
+		m_aux_matrix* players;
+	};
+
+	Containers::board* init_board(m_aux_matrix*);
+	Containers::players* init_players(m_aux_matrix*);
 
 	void play();
+
+	m_separated_arg*  aux_separate(char** t_argv);
 public:
-	init();
+	init(int t_argc, char** t_argv);
 	~init();
 
 };
