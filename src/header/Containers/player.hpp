3ea4b3b29146b04b1e5b3c298ad3a5b6dc6b4c36
Yowgf <alexthomasmol@gmail.com>
2020-09-02 09:52:59 -0300

    First version

--- src/header/Containers/player.hpp
+++ src/header/Containers/player.hpp
@@ -8,6 +8,9 @@
 //   that interacts with the board through valid
 //   movements, and keeps track of its own
 //   position.
+// In fact, the only things this class is
+//   responsible for is holding a player id and
+//   its initial position.
 /////////////////////////////////////////////////
 
 #ifndef PLAYER_H
@@ -15,7 +18,19 @@
 
 namespace Containers {
 
-class player {};
+class player {
+private:
+	unsigned int m_id;
+	unsigned int m_x;
+	unsigned int m_y;
+
+public:
+	player(unsigned int t_id, unsigned int t_x, unsigned int t_y);
+
+	unsigned int get_id();
+	unsigned int get_x();
+	unsigned int get_y();
+};
 
 }
 
